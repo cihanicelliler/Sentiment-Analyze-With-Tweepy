702c6c93849f0f1201c258908745dcc40135f9ba
Cihan İçelliler <icellilercihan@gmail.com>
2021-11-13 16:15:48 +0300

    v1.1 -tweets cleared. csv file created.

--- twittersent.py
+++ twittersent.py
@@ -6,6 +6,8 @@ from textblob import TextBlob
 from elasticsearch import Elasticsearch
 import re
 import string
+import csv
+import os
 
 # import twitter keys and tokens
 from config import *
@@ -19,6 +21,38 @@ else:
     print("Connection error!")
 
 
+def write_to_csv(tweet, dict_data, sentiment):
+    csvFile = open('tweetsResults.csv', 'a')
+    dict_data["text"] = clear_tweets(dict_data["text"])
+    csvWriter = csv.writer(csvFile)
+
+    if(os.stat(r"C:\Users\icell\Desktop\Programlama\Python\TwintForTwitter\tweepy\tweetsResults.csv").st_size == 0):
+        csvWriter.writerow(["Created At", "Source", "Tweet", "Sentiment",
+                            "Polarity", "Subjectivity", "User Name", "Place"])
+
+    # Write a row to the CSV file. I use encode UTF-8
+    csvWriter.writerow([dict_data["created_at"], dict_data["source"], dict_data["text"].encode('utf-8'), sentiment,
+                        tweet.sentiment.polarity, tweet.sentiment.subjectivity, dict_data["user"]["screen_name"], dict_data["place"]])
+    csvFile.close()
+
+
+def clear_tweets(tweet):
+    # clear Emoji patterns
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               "]+", flags=re.UNICODE)
+    tweet = emoji_pattern.sub(r'', tweet)
+
+    # clear punctions
+    tweet = tweet.translate(
+        str.maketrans('', '', string.punctuation))
+
+    return tweet
+
+
 class TweetStreamListener(StreamListener):
 
     # on success
@@ -27,39 +61,12 @@ class TweetStreamListener(StreamListener):
         # decode json
         dict_data = json.loads(data)
 
-        # HappyEmoticons
-        emoticons_happy = set([
-            ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
-            ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
-            '=-3', '=3', ':-))', ":'-)", ":')", ':*', ':^*', '>:P', ':-P', ':P', 'X-P',
-            'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)',
-            '<3'
-        ])
-
-        # Sad Emoticons
-        emoticons_sad = set([
-            ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
-            ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
-            ':c', ':{', '>:\\', ';('
-        ])
-
-        # clear emojis
-        emoji_pattern = re.compile("["
-                                   u"\U0001F600-\U0001F64F"  # emoticons
-                                   u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                                   u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                                   u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                   "]+", flags=re.UNICODE)
-        dict_data["text"] = emoji_pattern.sub(r'', dict_data["text"])
-
-        # clear punctions
-        dict_data["text"] = dict_data["text"].translate(
-            str.maketrans('', '', string.punctuation))
+        # call clear_tweets function
+        dict_data["text"] = clear_tweets(dict_data["text"])
 
         # pass tweet into TextBlob wihout reTweets
-        if (not dict_data["retweeted"]) and ('RT @' not in dict_data["text"]):
-            tweet = TextBlob(dict_data["text"])
-            print(tweet)
+        tweet = TextBlob(dict_data["text"])
+        print(tweet)
 
         # output sentiment polarity
         print(tweet.sentiment.polarity)
@@ -75,16 +82,17 @@ class TweetStreamListener(StreamListener):
         # output sentiment
         print(sentiment)
 
+        write_to_csv(tweet, dict_data, sentiment)
         # add text and sentiment info to elasticsearch
-        es.index(index="sentiment",
-                 doc_type="test-type",
-                 body={"author": dict_data["user"]["screen_name"],
-                       "date": dict_data["created_at"],
-                       "message": dict_data["text"],
-                       "polarity": tweet.sentiment.polarity,
-                       "subjectivity": tweet.sentiment.subjectivity,
-                       "sentiment": sentiment})
-        return True
+        # es.index(index="sentiment",
+        #          doc_type="test-type",
+        #          body={"author": dict_data["user"]["screen_name"],
+        #                "date": dict_data["created_at"],
+        #                "message": dict_data["text"],
+        #                "polarity": tweet.sentiment.polarity,
+        #                "subjectivity": tweet.sentiment.subjectivity,
+        #                "sentiment": sentiment})
+        # return True
 
     # on failure
     def on_error(self, status):
